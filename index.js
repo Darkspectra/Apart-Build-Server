628eddd771858855c8240723ef4ba90954080479
Darkspectra <ftanvirnovo@gmail.com>
2023-11-24 20:45:59 +0600

    store user sign UP information in Database

--- index.js
+++ index.js
@@ -32,12 +32,25 @@ async function run() {
     // await client.connect();
 
     const roomCollection = client.db("buildDB").collection("rooms");
+    const userCollection = client.db("buildDB").collection("users");
 
 
     app.get('/rooms', async (req, res) => {
         const result = await roomCollection.find().toArray();
         res.send(result);
       });
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
 
     // Send a ping to confirm a successful connection
